19bed41e4d31b30c3b40753be1381c486f6bf7c2
whj <emailwen@qq.com>
2021-11-30 15:56:54 +0800

    feat: 仓库初始化

---
+++ packages/components/new-table/index.ts
@@ -0,0 +1,6 @@
+import { withInstall } from '@element-plus/utils/with-install'
+import NewTable from './src/table.vue'
+
+export const ElNewTable = withInstall(NewTable)
+
+export default ElNewTable
---
+++ packages/hooks/use-global-config/index.ts
@@ -0,0 +1,12 @@
+import { inject } from 'vue'
+import { configProviderContextKey } from '@element-plus/tokens'
+import type { ConfigProvdierContext } from '@element-plus/tokens'
+
+const defaultConfig: ConfigProvdierContext = {
+  button: {
+    autoInsertSpace: false,
+  },
+}
+
+export const useGlobalConfig = () =>
+  inject(configProviderContextKey, defaultConfig)
